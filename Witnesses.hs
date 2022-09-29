590c3cec13e2bf907438a40f07acda2d289e3b34
Pascal Engel <ip4ssi@gmail.com>
2022-09-29 17:49:26 +0200

    finish basic witness generation (bug in meet)

--- Witnesses.hs
+++ Witnesses.hs
@@ -4,13 +4,44 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE RankNTypes #-}
-module Witnesses where
-import Control.Monad.State
-import Control.Monad.Except
-import Control.Monad.Writer
+{-# LANGUAGE StrictData #-}
+
+module Witnesses
+    ( main
+    , generateWitnesses
+    ) where
+
+import           Control.Monad.Except
+import           Control.Monad.State
+import           Data.Map                       ( Map )
+import qualified Data.Map                      as M
 
 main :: IO ()
-main = pure ()
+main = print
+    (generateWitnesses
+        [ Subtype Bot                          Top
+        , Subtype Nat                          (Inter Nat Top)
+        , Subtype (Union Int' (Inter Top Bot)) Int'
+        ]
+    )
+
+type SolverM a = StateT SolverState (Except String) a
+
+type Var = Char
+data SolverState = SolverState
+    { ss_todos :: Map Var Constraint
+    , -- ^ mapping from witness-var to constraint
+      ss_known :: Map Var (:<)
+    , -- ^ mapping from witness-var to constraint
+      ss_fresh :: [Var]
+      -- ^ "stream" of fresh variables
+    }
+
+runSolverM :: SolverM a -> Either String a
+runSolverM s = runExcept $ evalStateT s (SolverState M.empty M.empty ['a' ..])
+
+generateWitnesses :: [Constraint] -> Either String [(:<)]
+generateWitnesses cs = runSolverM (solve cs >>= substitute)
 
 data Polarity = Pos | Neg
 data Typ where
@@ -20,6 +51,7 @@ data Typ where
     Union     :: Typ -> Typ -> Typ
     Int'      :: Typ
     Nat       :: Typ
+ deriving Show
 
 data (:<) where
     Refl    :: (:<)
@@ -28,26 +60,43 @@ data (:<) where
     Meet    :: (:<) -> (:<) -> (:<)
     Join    :: (:<) -> (:<) -> (:<)
     Prim    :: (:<)
-    SubVar  :: Char -> (:<)
+    SubVar  :: Var -> (:<)
+ deriving Show
 
 data Constraint where
-    Subtype :: Typ -> Typ -> Constraint
-    InComplete :: Char -> Constraint -> Constraint
+    Subtype    :: Typ -> Typ -> Constraint
+ deriving Show
 
 solve :: [Constraint] -> SolverM [(:<)]
-solve [] = pure []
-solve ((InComplete v cnstr):cs) = do
-    solve (cnstr:cs)
-solve (cnstr:cs) = do
-    (foo,bar) <- solveSubWithWitness cnstr
-    tell [bar]
-    solve (foo++cs)
-
--- | Apply all substitutions to obtain complete witnesses (without witness-vars)
-applySubst :: [(:<)] -> [Constraint] -> [Constraint]
-applySubst = undefined
+solve []                          = pure []
+solve ((Subtype t s) : cs) = do
+    w <- solveSubWithWitness (Subtype t s)
+    todos <- gets ss_todos
+    known' <- mapM solveSubWithWitness todos
+    modify (\(SolverState todos known fresh) -> SolverState M.empty (M.union known' known) fresh)
+    cs <- solve cs
+    pure (w : cs)
 
 
+substitute :: [(:<)] -> SolverM [(:<)]
+substitute witnesses = do
+    -- m <- gets ss_todos
+    -- throwError (show m)
+    m <- gets ss_known
+    mapM (go m) witnesses
+  where
+    go :: Map Var (:<) -> (:<) -> SolverM (:<)
+    go m Refl = pure Refl
+    go m FromTop = pure FromTop
+    go m ToBot = pure ToBot
+    go m (Meet x0 x1) = Meet <$> go m x0 <*> go m x1
+    go m (Join x0 x1) = Join <$> go m x0 <*> go m x1
+    go m Prim = pure Prim
+    go m (SubVar c) = case M.lookup c m of
+         Nothing -> throwError $ "Cannot find witness variable: " <> show c <> " in env: " <> show m
+         Just w -> pure w
+
+-- | subconstraints for reference.
 solveSub :: Constraint -> [Constraint]
 solveSub (Subtype _ Top) = []
 solveSub (Subtype Bot _) = []
@@ -56,30 +105,27 @@ solveSub (Subtype (Union t s) r) = [Subtype t r, Subtype t s]
 solveSub (Subtype Nat Nat) = []
 solveSub (Subtype Int' Int') = []
 solveSub (Subtype Nat Int') = []
-solveSub _ = error "no"
+solveSub _ = error "Cannot solve constraint."
 
-type SolverM a = StateT [Char] (WriterT [(:<)] (Except String)) a
-
-runSolverM :: SolverM a -> Either String (a,[(:<)])
-runSolverM s = runExcept $ runWriterT $ evalStateT s ['a'..]
-
-solveSubWithWitness :: Constraint -> SolverM ([Constraint], (:<))
-solveSubWithWitness (Subtype _ Top) = pure ([], FromTop)
-solveSubWithWitness (Subtype Bot _) = pure ([], ToBot)
-solveSubWithWitness (Subtype t (Inter r s)) = do
-    foo <- get
-    let (var1:(var2:rest)) = foo -- ???
-    put rest
-    pure ([InComplete var1 $ Subtype t r, InComplete var2 $ Subtype t s], Meet (SubVar var1) (SubVar var2))
-solveSubWithWitness (Subtype (Union t s) r)  = do
-    foo <- get
-    let (var1:(var2:rest)) = foo -- ???
-    put rest
-    pure ([InComplete var1 $ Subtype t r, InComplete var2 $ Subtype t s], Join (SubVar var1) (SubVar var2))
-solveSubWithWitness (Subtype Nat Nat) = pure ([], Refl)
-solveSubWithWitness (Subtype Int' Int') = pure ([], Refl)
-solveSubWithWitness (Subtype Nat Int') = pure ([], Prim)
-solveSubWithWitness _ = throwError "no"
+solveSubWithWitness :: Constraint -> SolverM (:<)
+solveSubWithWitness (Subtype _   Top        ) = pure FromTop
+solveSubWithWitness (Subtype Bot _          ) = pure ToBot
+solveSubWithWitness (Subtype t   (Inter r s)) = do
+    foo <- gets ss_fresh
+    let (var1 : (var2 : rest)) = foo -- ???
+    let m' = M.fromList [(var1, Subtype t r), (var2, Subtype t s)]
+    modify (\(SolverState m d _) -> SolverState (M.union m' m) d rest)
+    pure $ Meet (SubVar var1) (SubVar var2)
+solveSubWithWitness (Subtype (Union t s) r) = do
+    foo <- gets ss_fresh
+    let (var1 : (var2 : rest)) = foo -- ???
+    let m' = M.fromList [(var1, Subtype t r), (var2, Subtype t s)]
+    modify (\(SolverState m d _) -> SolverState (M.union m' m) d rest)
+    pure $ Join (SubVar var1) (SubVar var2)
+solveSubWithWitness (Subtype Nat  Nat ) = pure Refl
+solveSubWithWitness (Subtype Int' Int') = pure Refl
+solveSubWithWitness (Subtype Nat  Int') = pure Prim
+solveSubWithWitness _                   = throwError "Cannot solve constraint."
 
 data Dictionary t where
     ShowDict :: Dictionary t
