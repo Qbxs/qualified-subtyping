0cb1818287caa60c338620d8176d87ab38d04ff3
Qbxs <ip4ssi@gmail.com>
2022-10-10 14:45:06 +0200

    remove todo-list

--- Witnesses.hs
+++ Witnesses.hs
@@ -11,7 +11,7 @@ import           Control.Monad.Except
 import           Control.Monad.State
 import           Data.Map                       ( Map )
 import qualified Data.Map                      as M
-import           Data.Tuple                     ( swap )
+import           Data.Tuple                     ( swap, uncurry )
 
 main :: IO ()
 main =
@@ -21,7 +21,7 @@ main =
                 , Subtype Nat (Inter (Inter Top Nat) Int')
                 , Subtype (Union Int' (Union Nat Bot)) Int'
                 , Subtype (FuncTy (Inter Top Int') Nat) (FuncTy Nat Int')
-                , Subtype (RecTy "a" (FuncTy Nat (RecVar "a"))) Top
+                , Subtype (RecTy "a" (FuncTy Nat (RecVar "a"))) (RecTy "a" (FuncTy Nat (RecVar "a")))
                 ]
         of
             Left  err -> putStrLn err
@@ -58,26 +58,22 @@ type Var = Char
 data SolverState = SolverState
     { ss_cache :: Map Constraint Var
      -- ^ already solved constraints, indirectly points to subtyping witnesses via ss_known
-    , ss_todos :: Map Var Constraint
-    , -- ^ mapping from witness-var to constraint
-      ss_known :: Map Var (:<)
-    , -- ^ mapping from witness-var to witness
-      ss_fresh :: [Var]
+    , ss_known :: Map Var (:<)
+     -- ^ mapping from witness-var to witness
+    , ss_fresh :: [Var]
       -- ^ "stream" of fresh variables
     }
  deriving Show
 
 runSolverM :: SolverM a -> Either String SolverState
-runSolverM s = snd <$> runExcept (runStateT s (SolverState M.empty M.empty M.empty ['a' ..]))
+runSolverM s = snd <$> runExcept (runStateT s (SolverState M.empty M.empty ['a' ..]))
 
 -- | Generate subtyping witnesses for subtyping constraints.
 generateWitnesses :: [Constraint] -> Either String (Map Constraint (:<))
-generateWitnesses cs = verify =<< runSolverM (solve cs >> substitute)
+generateWitnesses cs = composeCache <$> runSolverM (solve cs >> substitute)
   where
-    verify (SolverState cache todos known _)
-        | not (M.null todos) = Left ("Some todos are left:\n" <> show todos)
-        | otherwise          = Right (M.compose known cache)
-
+    composeCache (SolverState cache known _) = M.compose known cache
+       
 data Typ where
     Top    :: Typ
     Bot    :: Typ
@@ -109,33 +105,34 @@ data Constraint where
     Subtype :: Typ -> Typ -> Constraint
  deriving (Show, Eq, Ord)
 
+-- | Solve constraints, and write corresponding witnesses in cache.
 solve :: [Constraint] -> SolverM ()
 solve [] = pure ()
-solve (c : cs) = do
+solve (c : css) = do
     cacheHit <- inCache c
-    if cacheHit then solve cs else do
-      w <- solveSubWithWitness c
+    if cacheHit then solve css else do
       var <- freshVar
+      (w, cs) <- solveSubWithWitness c
       addToCache c var w
-      solveTodos
-      solve cs
+      mapM_ (uncurry solveWithVar) cs
+      solve css
 
--- | Solve todos recursively.
-solveTodos :: SolverM ()
-solveTodos = do
-    todos <- gets ss_todos
-    known' <- mapM solveSubWithWitness todos
-    modify $ \(SolverState cache todos' known fresh)
-            -> SolverState cache (M.difference todos' todos) (M.union known' known) fresh
-    unless (M.null todos) solveTodos
+-- | Same as /solve/ but don't generate a new witness var.
+solveWithVar :: Var -> Constraint -> SolverM ()
+solveWithVar var c = do
+    cacheHit <- inCache c
+    unless cacheHit $ do
+      (w, cs) <- solveSubWithWitness c
+      addToCache c var w
+      mapM_ (uncurry solveWithVar) cs
 
 -- | Substitute known witnesses for generated witness variables.
 substitute :: SolverM ()
 substitute = do
     ws <- gets ss_known
     coalesced <- mapM (go ws) ws
-    modify $ \(SolverState cache todos _ fresh)
-            -> SolverState cache todos coalesced fresh
+    modify $ \(SolverState todos _ fresh)
+            -> SolverState todos coalesced fresh
   where
     go :: Map Var (:<) -> (:<) -> SolverM (:<)
     go m (Refl ty) = pure (Refl ty)
@@ -168,35 +165,30 @@ solveSub _ = error "Cannot solve constraint."
 -- | Generate potentially incomplete witnesses
 -- using fresh witness variables in place for branches
 -- which will be substituted in later.
-solveSubWithWitness :: Constraint -> SolverM (:<)
-solveSubWithWitness (Subtype ty  Top        ) = pure (FromTop ty)
-solveSubWithWitness (Subtype Bot ty         ) = pure (ToBot ty)
+solveSubWithWitness :: Constraint -> SolverM ((:<), [(Var, Constraint)])
+solveSubWithWitness (Subtype ty  Top        ) = pure (FromTop ty, [])
+solveSubWithWitness (Subtype Bot ty         ) = pure (ToBot ty, [])
 solveSubWithWitness (Subtype t   (Inter r s)) = do
-    var1 <- freshVar
-    var2 <- freshVar
-    addTodos [(var1, Subtype t r), (var2, Subtype t s)]
-    pure $ Meet (SubVar var1) (SubVar var2)
+    c1@(var1, _) <- fromCacheOrFresh (Subtype t r)
+    c2@(var2, _) <- fromCacheOrFresh (Subtype t s)
+    pure (Meet (SubVar var1) (SubVar var2), [c1, c2])
 solveSubWithWitness (Subtype (Union t s) r) = do
-    var1 <- freshVar
-    var2 <- freshVar
-    addTodos [(var1, Subtype t r), (var2, Subtype s r)]
-    pure $ Join (SubVar var1) (SubVar var2)
+    c1@(var1, _) <- fromCacheOrFresh (Subtype t r)
+    c2@(var2, _) <- fromCacheOrFresh (Subtype s r)
+    pure (Join (SubVar var1) (SubVar var2), [c1, c2])
 solveSubWithWitness (Subtype ty@(RecTy _ _) ty') = do
-    var <- freshVar
-    addTodos [(var, Subtype (unfoldRecType ty) ty')]
-    pure (Rec (SubVar var))
+    c@(var, _) <- fromCacheOrFresh (Subtype (unfoldRecType ty) ty')
+    pure (Rec (SubVar var), [c])
 solveSubWithWitness (Subtype ty' ty@(RecTy _ _)) = do
-    var <- freshVar
-    addTodos [(var, Subtype ty' (unfoldRecType ty))]
-    pure (Rec (SubVar var))
+    c@(var, _) <- fromCacheOrFresh (Subtype ty' (unfoldRecType ty))
+    pure (Rec (SubVar var), [c])
 solveSubWithWitness (Subtype (FuncTy t s) (FuncTy t' s')) = do
-    var1 <- freshVar
-    var2 <- freshVar
-    addTodos [(var1, Subtype t' t), (var2, Subtype s s')]
-    pure $ Func (SubVar var1) (SubVar var2)
-solveSubWithWitness (Subtype Nat  Nat ) = pure (Refl Nat)
-solveSubWithWitness (Subtype Int' Int') = pure (Refl Int')
-solveSubWithWitness (Subtype Nat  Int') = pure Prim
+    c1@(var1, _) <- fromCacheOrFresh (Subtype t' t)
+    c2@(var2, _) <- fromCacheOrFresh (Subtype s s')
+    pure (Func (SubVar var1) (SubVar var2), [c1, c2])
+solveSubWithWitness (Subtype Nat  Nat ) = pure (Refl Nat, [])
+solveSubWithWitness (Subtype Int' Int') = pure (Refl Int', [])
+solveSubWithWitness (Subtype Nat  Int') = pure (Prim, [])
 solveSubWithWitness c                   = throwError $ "Cannot solve constraint:\n" <> show c
 
 -------------------------------------------------------------------------------
@@ -208,17 +200,20 @@ freshVar :: SolverM Var
 freshVar = do
     stream <- gets ss_fresh
     let (var : rest) = stream
-    modify $ \(SolverState cache todos known _)
-            -> SolverState cache todos known rest
+    modify $ \(SolverState todos known _)
+            -> SolverState todos known rest
     pure var
 
--- | Add elements to todo-map.
-addTodos :: [(Var, Constraint)] -> SolverM ()
-addTodos new = modify $ \(SolverState cache todos known fresh) -> SolverState
-    (M.union (M.fromList (swap <$> new)) cache)
-    (M.union (M.fromList new) todos)
-    known
-    fresh
+-- | Get already solved constraint or generate new var.
+fromCacheOrFresh :: Constraint -> SolverM (Var, Constraint)
+fromCacheOrFresh c = do
+    cache <- gets ss_cache
+    case M.lookup c cache of
+        Nothing -> do
+            var <- freshVar
+            pure (var, c)
+        Just var -> pure (var, c)
+
 
 -- | Check whether constraint is already solved.
 inCache :: Constraint -> SolverM Bool
@@ -226,9 +221,8 @@ inCache c =  gets $ M.member c . ss_cache
 
 -- | Add solved constraint to cache and known witnesses.
 addToCache :: Constraint -> Var -> (:<) -> SolverM ()
-addToCache c var w = modify $ \(SolverState cache todos known fresh) -> SolverState
+addToCache c var w = modify $ \(SolverState cache known fresh) -> SolverState
     (M.insert c var cache)
-    todos
     (M.insert var w known)
     fresh
 
